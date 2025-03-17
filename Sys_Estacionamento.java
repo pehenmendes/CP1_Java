8ddb82408082d2f837ce72957d1db50e93e84cdf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-17 11:39:44 -0300

    Correção de bugs

--- Sys_Estacionamento.java
+++ Sys_Estacionamento.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Sys_Estacionamento {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int operacao; int horario_entrada = 0; int horario_saida;
+        int operacao; int horario_entrada = 0;
         String marca = "", modelo = "", placa = "";
         do {
             System.out.print("*** MENU ESTACIONAMENTO ***\n1 - Cadastrar\n2 - Alterar\n3 - Consultar\n4 - Excluir\n5 - Calcular Estadia\n6 - Sair\n---> ");
             operacao = scanner.nextInt();
             if (operacao == 1) {
-                if (marca.isEmpty() || modelo.isEmpty() || placa.isEmpty() || !(horario_entrada > 5 && horario_entrada < 23)) {
+                if (marca.isEmpty() || modelo.isEmpty() || placa.isEmpty()) {
                     System.out.print("Diga a marca do veículo\n---> ");
                     marca = scanner.next();
                     System.out.print("Diga o modelo do veículo\n---> ");
@@ -62,31 +62,51 @@ public class Sys_Estacionamento {
                 }
             }
             else if (operacao == 4) {
-                marca = ""; modelo = ""; placa = ""; horario_entrada = 0;
-                System.out.print("Marca: " + marca + "\nModelo: " + modelo + "\nPlaca: " + placa + "\nHorário de entrada: " + horario_entrada);
-                System.out.println("Campos excluídos com sucesso!");
+                String pesquisa;
+                System.out.print("Qual a placa do carro que você deseja excluir o cadastro?\n---> ");
+                pesquisa = scanner.next();
+                if (pesquisa.equalsIgnoreCase(placa)) {
+                    marca = ""; modelo = ""; placa = ""; horario_entrada = 0;
+                    System.out.println("Marca: " + marca + "\nModelo: " + modelo + "\nPlaca: " + placa + "\nHorário de entrada: " + horario_entrada);
+                    System.out.println("Campos excluídos com sucesso!");
+                }
+                else {
+                    System.out.println("Veículo não encontrado!");
+                }
             }
             else if (operacao == 5) {
                 String pesquisa;
                 int valor_estadia = 0;
                 System.out.print("Qual a placa do carro que você deseja calcular o serviço?\n---> ");
                 pesquisa = scanner.next();
-                Random random = new Random();
                 if (pesquisa.equalsIgnoreCase(placa)) {
-                    horario_saida = random.nextInt(6, 22);
-                    if (horario_saida - horario_entrada >= 3) {
-                        valor_estadia = 30;
+                    if (horario_entrada == 0) {
+                        System.out.println("Erro: O veículo não foi cadastrado corretamente!");
                     }
-                    else if (horario_saida - horario_entrada >= 5) {
-                        valor_estadia = 45;
+                    else {
+                        Random random = new Random();
+                        int horario_saida = random.nextInt(6, 23);
+                        int tempo_estadia = horario_saida - horario_entrada;
+                        if (tempo_estadia < 0) {
+                            tempo_estadia = -tempo_estadia;
+                        }
+                        if (tempo_estadia == 0) {
+                            System.out.println("PASSAGEM LIBERADA!");
+                        }
+                        else if (tempo_estadia >= 24) {
+                            valor_estadia = 80;
+                        }
+                        else if (tempo_estadia >= 7) {
+                            valor_estadia = 60;
+                        }
+                        else if (tempo_estadia >= 5) {
+                            valor_estadia = 45;
+                        }
+                        else if (tempo_estadia >= 3) {
+                            valor_estadia = 30;
+                        }
+                        System.out.println("O carro com placa " + placa + " esteve no estacionamento por " + tempo_estadia + " horas. O valor da estadia é R$ " + valor_estadia);
                     }
-                    else if (horario_saida - horario_entrada >= 7) {
-                        valor_estadia = 60;
-                    }
-                    else if (horario_saida - horario_entrada >= 24) {
-                        valor_estadia = 80;
-                    }
-                    System.out.println("O carro com placa " + placa + " precisa pagar R$ " + valor_estadia);
                 }
                 else {
                     System.out.println("Veículo não encontrado!");
