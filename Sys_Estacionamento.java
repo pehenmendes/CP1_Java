a17255d7e8f06a58ee621f77757f43b5daff8c2a
Kayky Stiliano <145211344+KStiliano@users.noreply.github.com>
2025-03-21 08:56:22 -0300

    Toques finais

--- Sys_Estacionamento.java
+++ Sys_Estacionamento.java
@@ -4,123 +4,116 @@ import java.util.Scanner;
 public class Sys_Estacionamento {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int operacao; int horario_entrada = 0;
-        String marca = "", modelo = "", placa = "";
+        int operacao, horario_entrada = 0;
+        String marca = "", modelo = "", placa = "", pesquisa = "";
         do {
             System.out.print("*** MENU ESTACIONAMENTO ***\n1 - Cadastrar\n2 - Alterar\n3 - Consultar\n4 - Excluir\n5 - Calcular Estadia\n6 - Sair\n---> ");
             operacao = scanner.nextInt();
-            if (operacao == 1) {
-                if (marca.isEmpty() || modelo.isEmpty() || placa.isEmpty()) {
-                    System.out.print("Diga a marca do veículo\n---> ");
-                    marca = scanner.next();
-                    System.out.print("Diga o modelo do veículo\n---> ");
-                    modelo = scanner.next();
-                    System.out.print("Diga a placa do veículo\n---> ");
-                    placa = scanner.next();
-                    System.out.print("Diga o horario de entrada do veículo\n---> ");
-                    horario_entrada = scanner.nextInt();
-                    System.out.println("Cadastro realizado com sucesso!");
-                }
-                else {
-                    System.out.println("Sem espaço no banco de dados!");
-                }
-            }
-            else if (operacao == 2) {
-                String escolha, pesquisa;
-                System.out.print("Qual a placa do carro que você deseja alterar o cadastro?\n---> ");
-                pesquisa = scanner.next();
-                if (pesquisa.equalsIgnoreCase(placa)) {
-                    System.out.print("Qual campo você deseja alterar? (marca, modelo ou horário)\n---> ");
-                    escolha = scanner.next();
-                    if (escolha.equalsIgnoreCase("marca")) {
-                        System.out.print("Diga a marca a ser alterado\n---> ");
+            switch (operacao) {
+                case 1:
+                    if (marca.isEmpty() || modelo.isEmpty() || placa.isEmpty()) {
+                        System.out.print("Diga a marca do veículo\n---> ");
                         marca = scanner.next();
-                    }
-                    else if (escolha.equalsIgnoreCase("modelo")) {
-                        System.out.print("Diga o modelo a ser alterado\n---> ");
+                        System.out.print("Diga o modelo do veículo\n---> ");
                         modelo = scanner.next();
-                    }
-                    else if (escolha.equalsIgnoreCase("horario")) {
-                        System.out.print("Diga o horário a ser alterado\n---> ");
+                        System.out.print("Diga a placa do veículo\n---> ");
+                        placa = scanner.next();
+                        System.out.print("Diga o horário de entrada do veículo\n---> ");
                         horario_entrada = scanner.nextInt();
-                    }
-                    System.out.println("Alteração cadastrada com sucesso!");
-                }
-                else {
-                    System.out.println("Placa inválida!");
-                }
-            }
-            else if (operacao == 3) {
-                String pesquisa;
-                System.out.print("Qual a placa do carro que você deseja alterar o cadastro?\n---> ");
-                pesquisa = scanner.next();
-                if (pesquisa.equalsIgnoreCase(placa)) {
-                    System.out.println("Marca: " + marca + "\nModelo: " + modelo + "\nPlaca: " + placa + "\nHorário de entrada: " + horario_entrada);
-                }
-                else {
-                    System.out.println("Veículo não encontrado!");
-                }
-            }
-            else if (operacao == 4) {
-                String pesquisa;
-                System.out.print("Qual a placa do carro que você deseja excluir o cadastro?\n---> ");
-                pesquisa = scanner.next();
-                if (pesquisa.equalsIgnoreCase(placa)) {
-                    marca = ""; modelo = ""; placa = ""; horario_entrada = 0;
-                    System.out.println("Marca: " + marca + "\nModelo: " + modelo + "\nPlaca: " + placa + "\nHorário de entrada: " + horario_entrada);
-                    System.out.println("Campos excluídos com sucesso!");
-                }
-                else {
-                    System.out.println("Veículo não encontrado!");
-                }
-            }
-            else if (operacao == 5) {
-                String pesquisa;
-                int valor_estadia = 0;
-                System.out.print("Qual a placa do carro que você deseja calcular o serviço?\n---> ");
-                pesquisa = scanner.next();
-                if (pesquisa.equalsIgnoreCase(placa)) {
-                    if (horario_entrada == 0) {
-                        System.out.println("Erro: O veículo não foi cadastrado corretamente!");
+                        System.out.println("Cadastro realizado com sucesso!");
                     }
                     else {
-                        Random random = new Random();
-                        int horario_saida = random.nextInt(6, 23);
-                        int tempo_estadia = horario_saida - horario_entrada;
-                        if (tempo_estadia < 0) {
-                            tempo_estadia = -tempo_estadia;
-                        }
-                        if (tempo_estadia == 0) {
-                            System.out.println("PASSAGEM LIBERADA!");
-                        }
-                        else if (tempo_estadia >= 24) {
-                            valor_estadia = 80;
-                        }
-                        else if (tempo_estadia >= 7) {
-                            valor_estadia = 60;
-                        }
-                        else if (tempo_estadia >= 5) {
-                            valor_estadia = 45;
-                        }
-                        else if (tempo_estadia >= 3) {
-                            valor_estadia = 30;
-                        }
-                        System.out.println("O carro com placa " + placa + " esteve no estacionamento por " + tempo_estadia + " horas. O valor da estadia é R$ " + valor_estadia);
+                        System.out.println("Sem espaço no banco de dados!");
                     }
-                }
-                else {
-                    System.out.println("Veículo não encontrado!");
-                }
+                    break;
+                case 2:
+                    System.out.print("Qual a placa do carro que você deseja alterar o cadastro?\n---> ");
+                    pesquisa = scanner.next();
+                    if (pesquisa.equalsIgnoreCase(placa)) {
+                        System.out.print("Qual campo você deseja alterar? (marca, modelo ou horário)\n---> ");
+                        String escolha = scanner.next();
+                        switch (escolha.toLowerCase()) {
+                            case "marca":
+                                System.out.print("Diga a nova marca\n---> ");
+                                marca = scanner.next();
+                                break;
+                            case "modelo":
+                                System.out.print("Diga o novo modelo\n---> ");
+                                modelo = scanner.next();
+                                break;
+                            case "horario":
+                                System.out.print("Diga o novo horário de entrada\n---> ");
+                                horario_entrada = scanner.nextInt();
+                                break;
+                            default:
+                                System.out.println("Opção inválida!");
+                        }
+                        System.out.println("Alteração cadastrada com sucesso!");
+                    }
+                    else {
+                        System.out.println("Placa inválida!");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Qual a placa do carro que você deseja consultar?\n---> ");
+                    pesquisa = scanner.next();
+                    if (pesquisa.equalsIgnoreCase(placa)) {
+                        System.out.println("Marca: " + marca + "\nModelo: " + modelo + "\nPlaca: " + placa + "\nHorário de entrada: " + horario_entrada);
+                    }
+                    else {
+                        System.out.println("Veículo não encontrado!");
+                    }
+                    break;
+                case 4:
+                    System.out.print("Qual a placa do carro que você deseja excluir o cadastro?\n---> ");
+                    pesquisa = scanner.next();
+                    if (pesquisa.equalsIgnoreCase(placa)) {
+                        marca = ""; modelo = ""; placa = ""; horario_entrada = 0;
+                        System.out.println("Cadastro excluído com sucesso!");
+                    }
+                    else {
+                        System.out.println("Veículo não encontrado!");
+                    }
+                    break;
+                case 5:
+                    System.out.print("Qual a placa do carro que você deseja calcular o serviço?\n---> ");
+                    pesquisa = scanner.next();
+                    if (pesquisa.equalsIgnoreCase(placa)) {
+                        if (horario_entrada == 0) {
+                            System.out.println("Erro: O veículo não foi cadastrado corretamente!");
+                        }
+                        else {
+                            Random random = new Random();
+                            int valor_estadia = 0;
+                            int horario_saida = random.nextInt(6, 23);
+                            int tempo_estadia = horario_saida - horario_entrada;
+                            if (tempo_estadia < 0) {
+                                tempo_estadia = -tempo_estadia;
+                            }
+                            if (tempo_estadia == 0) {
+                                System.out.println("PASSAGEM LIBERADA!");
+                            } else if (tempo_estadia >= 24) {
+                                valor_estadia = 80;
+                            } else if (tempo_estadia >= 7) {
+                                valor_estadia = 60;
+                            } else if (tempo_estadia >= 5) {
+                                valor_estadia = 45;
+                            } else if (tempo_estadia >= 3) {
+                                valor_estadia = 30;
+                            }
+                            System.out.println("O carro com placa " + placa + " esteve no estacionamento por " + tempo_estadia + " horas. O valor da estadia é R$ " + valor_estadia);
+                        }
+                    } else {
+                        System.out.println("Veículo não encontrado!");
+                    }
+                    break;
+                case 6:
+                    System.out.println("Saindo do programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
             }
-            else if (operacao == 6) {
-                System.out.println("Saindo do programa...");
-                break;
-            }
-            else {
-                System.out.println("Opção inválida!");
-            }
-        }
-        while (true);
+        } while (operacao != 6);
         scanner.close();
     }
 }
